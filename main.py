0add8f008204a1a3e2e1c7cd36f8402652b52dcc
xElon-Mask <xelonmask@protonmail.com>
2022-09-07 13:42:51 +0200

    add while loop to convert multiple user's values

--- main.py
+++ main.py
@@ -5,6 +5,10 @@
 # 3 - Afficher la valeur convertie (et l'unité : cm ou pouces)
 
 
+# Optionnel :
+
+# Boucler pour convertir plusieurs valeurs (en conservant toujours le même sens de conversion), et proposer une option pour sortir du programme.
+
 
 pouce_en_cm = 2.54
 
@@ -22,15 +26,20 @@ def conversion(unite):
         value_convert_pouce = value_int * cm_en_pouce
         return print("Cela donne " + str(value_convert_pouce) + f" {unite}")
 
-   
+start_program = True
+
+while start_program == True:
+    choix_user = input("Souhaitez-vous convertir des pouces en cm, ou des cm en pouces ? Choisissez le numéro correspondant : 1 - Convertir des pouces en cm 2 - Convertir des cm en pouces 3 - Sortir du programme") 
+    if choix_user == "1":
+        conversion("cm")
+    elif choix_user == "2":
+        conversion("pouces")
+    elif choix_user == "3":
+        print("Programme interrompu par l'utilisateur")
+        print("Fin du programme")
+        start_program = False
+    else:
+        print("ERREUR: Vous ne pouvez rentrer que 1, 2 ou 3")
 
 
-choix_user = input("Souhaitez-vous convertir des pouces en cm, ou des cm en pouces ? Choisissez le numéro correspondant : 1 - Convertir des pouces en cm 2 - Convertir des cm en pouces")
-if choix_user == "1":
-    conversion("cm")
-elif choix_user == "2":
-    conversion_pouce("pouces")
-else:
-    print("ERREUR: Vous ne pouvez rentrer que 1 ou 2")
 
-print("Fin du programme")
