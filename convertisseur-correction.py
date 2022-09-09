5115a378114184d98478aa0bcee9ef248cf37a76
xElon-Mask <xelonmask@protonmail.com>
2022-09-09 12:25:16 +0200

    create while loop to user's request to convert their unit's choice + add exit optionin the program

--- convertisseur-correction.py
+++ convertisseur-correction.py
@@ -3,12 +3,23 @@
 1 cm = 0,394 pouces
 """
 
+"""
+effectuer_conversion : Cette fonction convertit les unités unit1 vers unit2
+
+Return :
+True : L'utilisateur ne veut plus convertir
+False : L'utilisateur a donné une valeur à convertir
+"""
+
 # Conversion de unit1 vers unit2
 def effectuer_conversion(unit1: str, unit2: str, facteur: float):
-    valeur_str = input(f"Conversion {unit1} -> {unit2}. Donnez la valeur en {unit1} : ")
+    valeur_str = input(f"Conversion {unit1} -> {unit2}. Donnez la valeur en {unit1} (ou 'q' pour quitter) : ")
+    if valeur_str == "q":
+        return True
     valeur_float = float(valeur_str)
     valeur_convertie = round(valeur_float * facteur, 2)
     print(f"Résultat de la conversion : {valeur_float} {unit1} = {valeur_convertie} {unit2}")
+    return False
 
 print("Ce programme vous permet d'effectuer des conversions d'unités")
 print("1 - Pouces vers cm")
@@ -18,3 +29,16 @@ if choice == "1":
     effectuer_conversion("pouces", "cm", 2.54)
 if choice == "2":
     effectuer_conversion("cm", "pouces", 0.394)
+
+
+"""
+if effectuer_conversion("pouces", "cm", 2.54) == True, alors on sort de la boucle avec break car la fonction renvoie True lorsque
+l'utilisateur à rentrer q pour quitter. En python le paramètre par défaut est True, donc pas besoin de le préciser pour la condition du if
+"""
+while True:
+    if choice == "1":
+        if effectuer_conversion("pouces", "cm", 2.54):
+            break
+    if choice == "2":
+        if effectuer_conversion("cm", "pouces", 0.394):
+            break
